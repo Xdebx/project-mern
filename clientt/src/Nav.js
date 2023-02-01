58c4331a39356b220e8b0485025a74ef9e97fbd1
Xdebx <86518562+Xdebx@users.noreply.github.com>
2023-02-01 17:38:10 +0800

    Fetch/Login/Logout

--- clientt/src/Nav.js
+++ clientt/src/Nav.js
@@ -1,30 +1,38 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getUser, logout } from './Helpers';
 
-import { Link } from 'react-router-dom';
-
-
-const Nav = () => (
-
-    <nav>
-
-        <ul className="nav nav-tabs">
-
-            <li className="nav-item pr-3 pt-3 pb-3">
-
-                <Link to="/">Home</Link>
-
-            </li>
-
-            <li className="nav-item pr-3 pt-3 pb-3">
-
-                <Link to="/create">Create</Link>
-
-            </li>
-
-        </ul>
-
-    </nav>
-
-);
+const Nav = () => {
+    let navigate = useNavigate();
+    return (
+        <nav>
+            <ul className="nav nav-tabs">
+                <li className="nav-item pr-3 pt-3 pb-3">
+                    <Link to="/">Home</Link>
+                </li>
+                <li className="nav-item pr-3 pt-3 pb-3">
+                    <Link to="/create">Create</Link>
+                </li>
+                {/* <li className="nav-item ml-auto pr-3 pt-3 pb-3">
+                <Link to="/login">Login</Link>
+            </li> */}
+                {/* CONDITIONAL RENDERING */}
+                {!getUser() && (
+                    <li className="nav-item ml-auto pr-3 pt-3 pb-3">
+                        <Link to="/login">Login</Link>
+                    </li>
+                )}
+                {getUser() && (
+                    <li
+                        onClick={() => logout(() => navigate('/'))}
+                        className="nav-item ml-auto pr-3 pt-3 pb-3"
+                        style={{ cursor: 'pointer' }}>
+                        Logout
+                    </li>
+                )}
+            </ul>
+        </nav >
+    );
+};
 
 export default Nav;
--- server/routes/post.js
+++ server/routes/post.js
@@ -3,10 +3,18 @@ const router = express.Router();
 
 // import controller methods
 
-const { create, list } = require('../controllers/post');
+const { create, list, read, update, remove } = require('../controllers/post');
+const { requireSignin } = require('../controllers/auth');
 
+router.post('/post', requireSignin, create);
+// router.post('/post', create);
 router.get('/posts', list);
-
-router.post('/post', create);
-
+router.get('/post/:slug', read);
+router.put('/post/:slug', update);
+router.delete('/post/:slug', remove);
+router.get('/secret', requireSignin, (req, res) => {
+    res.json({
+        data: req.user.name
+    });
+});
 module.exports = router;
